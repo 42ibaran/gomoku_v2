fe4960dc4e6b405902b4892524595a9a0edef38a
Cristina Fernández Bornay <cristinafernandezbornay@outlook.com>
2021-12-02 13:46:08 +0000

    merge conflics

--- launch_game.py
+++ launch_game.py
@@ -1,10 +1,12 @@
 import argparse
+from algo.maximilian import get_next_move
 from algo.board import load_hashtables, save_hashtables
 from algo.constants import WHITE, BLACK, EMPTY, COLOR_DICTIONARY
-from algo.errors import YouAreDumbException
+from algo.errors import ForbiddenMoveError
 from algo.game import Game
 from algo.maximilian import Maximilian
 from algo.move import Move
+import cProfile
 
 def get_arguments():
     parser = argparse.ArgumentParser()
@@ -22,9 +24,9 @@ def get_human_move(game, last_move=None):
     try:
         human_move = Move(move_color, move_position)
         if game.board.matrix[human_move.position] != EMPTY:
-            raise YouAreDumbException("The cell is already taken you dum-dum.")
+            raise ForbiddenMoveError("The cell is already taken you dum-dum.")
         return human_move
-    except YouAreDumbException as e:
+    except ForbiddenMoveError as e:
         print(e)
         return get_human_move(game, last_move)
 
@@ -46,17 +48,16 @@ def play_in_terminal(human_vs_maximilian, suggestion, human_as_white, intelligen
     print(intelligent)
     last_move = None
     game = Game()
-    maximilian = Maximilian()
     human_turn = False if human_as_white else True
     turn = 0
     while True:
         print_turn(human_turn, last_move)
         if human_vs_maximilian and not human_turn:
-            last_move, time_maximilian = maximilian.get_next_move(game.board)
+            last_move, time_maximilian = get_next_move(game.board)
             print_maximilian_move(last_move.position, time_maximilian)
         else:
             if suggestion:
-                suggestion_maximilian, _ = maximilian.get_next_move(game.board)
+                suggestion_maximilian, _ = get_next_move(game.board)
                 print_maximilian_suggestion(suggestion_maximilian.position)
             last_move = get_human_move(game, last_move)
         game.record_new_move(last_move)
