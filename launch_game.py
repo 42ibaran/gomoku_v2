3450f2510c5d0edd11fa2888983b95bb46b1732b
Cristina Fernández Bornay <cristinafernandezbornay@outlook.com>
2021-12-02 13:30:03 +0000

    feat(launch_game): adding new args

--- launch_game.py
+++ launch_game.py
@@ -1,6 +1,4 @@
 import argparse
-import sys
-import time
 from algo.board import load_hashtables, save_hashtables
 from algo.constants import WHITE, BLACK, EMPTY, COLOR_DICTIONARY
 from algo.errors import YouAreDumbException
@@ -10,24 +8,13 @@ from algo.move import Move
 
 def get_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--terminal", action='store_true', help="Use the terminal.")
+    parser.add_argument("-t", "--terminal", action='store_true', help="Play on the terminal.")
     parser.add_argument("-m", "--maximilian", action='store_true', help="Play against Maximilian.")
-    parser.add_argument("-s", "--suggestion", action='store_true', help="Receive suggestions.")
+    parser.add_argument("-s", "--suggestion", action='store_true', help="Receive Maximilian' suggestions.")
     parser.add_argument("-w", "--white", action='store_true', help="Play as white (2nd turn).")
+    parser.add_argument("-i", "--intelligent", action='store_true', help="Use The I.Max aka. The Intelligent Maximilian\n[default option The MP.Max. aka. The Max Power Maximilian]")
     arguments = parser.parse_args()
-    human_vs_maximilian = arguments.maximilian
-    terminal = arguments.terminal
-    suggestion = arguments.suggestion
-    if not terminal:
-        info = input("Would you like to use the terminal? [yes/y] ")
-        terminal = True if info == "y" else False
-    if not human_vs_maximilian:
-        info = input("Would you like to play against Maximilian? [yes/y] ")
-        human_vs_maximilian = True if info == "y" else False
-    if not suggestion:
-        info = input("Would you like to receive suggestions? [yes/y] ")
-        suggestion = True if info == "y" else False
-    return terminal, human_vs_maximilian, suggestion, arguments.white if arguments.white else False
+    return arguments.terminal, arguments.maximilian, arguments.suggestion, arguments.white, arguments.intelligent
 
 def get_human_move(game, last_move=None):
     move_position = input("Where would you like to play? <pos_y pos_x> : ")
@@ -41,6 +28,11 @@ def get_human_move(game, last_move=None):
         print(e)
         return get_human_move(game, last_move)
 
+def print_turn(human_turn, last_move):
+    print("\n=================== [{}][{}]s TURN ===================\n".format(
+            COLOR_DICTIONARY[last_move.opposite_color if last_move else BLACK],
+            "HUMAN" if human_turn else "MAXIM"))
+
 def print_maximilian_move(position, time):
     print("Maximilian's move: {}\nTime: {}".format(position, time))
 
@@ -50,16 +42,15 @@ def print_maximilian_suggestion(position):
 def game_over_bitch():
     print("It's over bitch.")
 
-def play_in_terminal(human_as_white, human_vs_maximilian, suggestion):
+def play_in_terminal(human_vs_maximilian, suggestion, human_as_white, intelligent):
+    print(intelligent)
     last_move = None
     game = Game()
     maximilian = Maximilian()
     human_turn = False if human_as_white else True
     turn = 0
     while True:
-        print("\n=================== [{}][{}]s TURN ===================\n".format(
-                "HUMAN" if human_turn else "MAXIM",
-                COLOR_DICTIONARY[last_move.opposite_color if last_move else BLACK]))
+        print_turn(human_turn, last_move)
         if human_vs_maximilian and not human_turn:
             last_move, time_maximilian = maximilian.get_next_move(game.board)
             print_maximilian_move(last_move.position, time_maximilian)
@@ -76,19 +67,15 @@ def play_in_terminal(human_as_white, human_vs_maximilian, suggestion):
         human_turn = not human_turn if human_vs_maximilian else True
         turn += 1 if last_move.color == WHITE else 0
 
-def play_in_web(game, human_vs_maximilian, maximilian):
-    print("LOOOOOOOOL")
-
 if __name__ == "__main__":
-    terminal, human_vs_maximilian, suggestion, human_as_white = get_arguments()
-    play_by_option = {
-        True: play_in_terminal,
-        False: play_in_web
-    }
-    try:
-        load_hashtables()
-        play_by_option[terminal](human_as_white, human_vs_maximilian, suggestion)
-    except KeyboardInterrupt:
-        pass
-    save_hashtables()
+    terminal, human_vs_maximilian, suggestion, human_as_white, intelligent = get_arguments()
+    if (terminal):
+        try:
+            load_hashtables()
+            play_in_terminal(human_vs_maximilian, suggestion, human_as_white, intelligent)
+        except KeyboardInterrupt:
+            pass
+        save_hashtables()
+    else:
+        print("LAUNCH GAME ON WEB HEHE BISOUS :)")
     print("\nGood bye!")
